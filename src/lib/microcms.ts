dd2dc8952abf47b9144560329459461b0aaba2da
BENwada <domo0103wada@gmail.com>
2025-07-03 18:21:55 +0900

    microCMS fetch

---
+++ src/lib/microcms.ts
@@ -0,0 +1,8 @@
+import { createClient } from "microcms-js-sdk";
+
+const client = createClient({
+  serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.MICROCMS_API_KEY,
+});
+
+export default client;
